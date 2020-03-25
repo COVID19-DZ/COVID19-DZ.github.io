341b15c8df59610c8f143d4b942dc5298676df70
COVID19-DZ <62350789+COVID19-DZ@users.noreply.github.com>
2020-03-26 00:23:44 +0800

    v56

--- functions.js
+++ functions.js
@@ -26,7 +26,9 @@ jQuery(document).ready(function () {
             31: '#FFE5CC',  //Oran
             38: '#FFE5CC',   // Tissemsilt
             22: '#FFE5CC',   // sba
-            40: '#FFE5CC'   // khanshla
+            40: '#FFE5CC',   // khanshla
+            42: '#FFE5CC',   // khanshla
+             25: '#FFE5CC'   // khanshla
         },
         backgroundColor: '#fff',
         onRegionClick: function (element, code, region) {
