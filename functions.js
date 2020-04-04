97a30479ab9f68fe8420c9f467f1bf21a289e68b
COVID19-DZ <62350789+COVID19-DZ@users.noreply.github.com>
2020-04-05 01:08:08 +0800

    V89

--- functions.js
+++ functions.js
@@ -49,6 +49,7 @@ jQuery(document).ready(function () {
             33: '#FFE5CC',   // ilizi
             3: '#FFE5CC',   // aghwat
             14: '#FFE5CC'   // Tiart
+            43: '#FFE5CC'   // mila
         },
         backgroundColor: '#fff',
         onRegionClick: function (element, code, region) {
